9fcd6b376378668fc48cb3ca73db2d870bfdb256
Mikipareja <mikipareja@gmail.com>
2024-06-20 19:09:54 +0200

    first commit

--- server.js
+++ server.js
@@ -5,7 +5,19 @@ const dotenv = require('dotenv');
 dotenv.config({path:'./config/config.env'});
 
 //crear una app a partir de la clase express
-const app = express();
+c = express();
+
+//especificar el archivo de rutas campus.js
+const campus = require('./routes/campus');
+const alumnos = require('./routes/alumnos');
+
+
+//montar (activar) rutas
+app.use('api/v1campus', campus);
+
+app.use('/api/v1/', alumnos)
+
+
 
 //establecer el puerto en localhost
 const PORT = process.env.PORT || 5000;
